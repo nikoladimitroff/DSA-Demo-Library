ac950e448cf7d508ca0fe4929e613cccdce3c88b
PreskoIsTheGreatest <preskosemov@abv.bg>
2015-01-11 21:44:03 +0200

    Homework 2 BookIndexer

---
+++ homeworks/IBookIndexer.java
@@ -0,0 +1,10 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bookindexer61654;
+
+public interface IBookIndexer {
+      void buildIndex(String bookFilePath, String[] keywords, String indexFilePath);
+}
